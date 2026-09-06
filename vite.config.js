39a54c414fb1d5bf5ca161ca928c5b2aeb9d6224
Thiện Lâm <lamminhthien02012000@gmail.com>
2026-09-06 22:15:18 +0700

    Fix show wrong timezone build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
 
+const now = new Date();
+const offset = -now.getTimezoneOffset() / 60;
+const timezone = `UTC${offset >= 0 ? '+' : ''}${offset}`;
+const buildTime = `${now.toLocaleString()} (${timezone})`;
+
 export default defineConfig({
   define: {
-    __BUILD_TIME__: JSON.stringify(new Date().toLocaleString()),
+    __BUILD_TIME__: JSON.stringify(buildTime),
   },
 });
